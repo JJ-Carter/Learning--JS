696ecd1de0c6a387ede8a3277b22da57a4fe3b8f
Jesse Carter <35956744+JJ-Carter@users.noreply.github.com>
2018-06-15 04:00:48 -0400

    Create funcAddArray.js

---
+++ funcAddArray.js
@@ -0,0 +1,10 @@
+// Define friends 
+var myFriends = ["Jesse", "Jessie", "Jessica"];
+
+// Function to get the number of friends then add a new friend
+function addFriend(name) {
+  var num = myFriends.length
+  myFriends[num] = name;
+}
+
+addFriend("Jess");
